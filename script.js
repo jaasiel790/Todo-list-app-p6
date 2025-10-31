badbfe903360fff101cff2a9b1aee349b9f9f31b
jaasiel790 <amirezjaasiel@gmail.com>
2025-10-31 21:49:23 +0000

    make add task add a task

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Script started")
+console.log("Script started");
+
+
+// add a task
+function addTask() {
+    //get task text
+    let textbox = document.getElementById("task-input");
+    let taskText = textbox.value;
+    textbox.value = "";
+
+    //get tasklist
+    let taskList = document.getElementById("task-list");
+
+    //generate id number
+    let idNum = taskList.childElementCount
+
+    // create task div
+     let taskDiv = document.createElement("div");
+     taskDiv.id = "task" + taskList.childElementCount;
+    taskDiv.classList.add("task-item")
+    if (idNum % 2 == 1){
+        taskDiv.style.backgroundColor = "turquoise";
+    }
+    // create checkbox
+     let checkbox = document.createElement("input")
+     checkbox.type = "checkbox";
+     checkbox.id = "checkbox" + taskList.childElementCount;
+
+    //create label
+    let label = document.createElement("label")
+    label.id = "label" + taskList.childElementCount;
+    //set label text
+    label.innerText = taskText;
+    //add the checkbox to the task div
+    taskDiv.appendChild(checkbox);
+    //add the label to the task div
+    taskDiv.appendChild(label);
+    // add the task div to the list
+    taskList.appendChild(taskDiv);
+}
