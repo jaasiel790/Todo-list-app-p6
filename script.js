78bcd80c0b0915a0b21de6a2bad5b80447fd0562
jaasiel790 <amirezjaasiel@gmail.com>
2025-11-05 21:00:49 +0000

    local storage progress

--- script.js
+++ script.js
@@ -8,36 +8,8 @@ function addTask() {
     let taskText = textbox.value;
     textbox.value = "";
 
-    //get tasklist
-    let taskList = document.getElementById("task-list");
+    createTask(taskText);
 
-    //generate id number
-    let idNum = taskList.childElementCount
-
-    // create task div
-     let taskDiv = document.createElement("div");
-     taskDiv.id = "task" + taskList.childElementCount;
-    taskDiv.classList.add("task-item")
-    if (idNum % 2 == 1){
-        taskDiv.style.backgroundColor = "turquoise";
-    }
-    // create checkbox
-     let checkbox = document.createElement("input")
-     checkbox.type = "checkbox";
-     checkbox.id = "checkbox" + taskList.childElementCount;
-     checkbox.addEventListener("change", removeTask);
-
-    //create label
-    let label = document.createElement("label")
-    label.id = "label" + taskList.childElementCount;
-    //set label text
-    label.innerText = taskText;
-    //add the checkbox to the task div
-    taskDiv.appendChild(checkbox);
-    //add the label to the task div
-    taskDiv.appendChild(label);
-    // add the task div to the list
-    taskList.appendChild(taskDiv);
 }
 
 
@@ -69,3 +41,51 @@ function fixColor() {
     }
 }
 
+function createTask(taskText) {
+   //get tasklist
+    let taskList = document.getElementById("task-list");
+
+    //generate id number
+    let idNum = taskList.childElementCount
+
+    // create task div
+     let taskDiv = document.createElement("div");
+     taskDiv.id = "task" + taskList.childElementCount;
+    taskDiv.classList.add("task-item")
+    if (idNum % 2 == 1){
+        taskDiv.style.backgroundColor = "turquoise";
+    }
+    // create checkbox
+     let checkbox = document.createElement("input")
+     checkbox.type = "checkbox";
+     checkbox.id = "checkbox" + taskList.childElementCount;
+     checkbox.addEventListener("change", removeTask);
+
+    //create label
+    let label = document.createElement("label")
+    label.id = "label" + taskList.childElementCount;
+    //set label text
+    label.innerText = taskText;
+
+    //save task to local storage
+    localStorage.setItem(taskDiv.id, taskText);
+    console.log(localStorage.length)
+
+    //add the checkbox to the task div
+    taskDiv.appendChild(checkbox);
+    //add the label to the task div
+    taskDiv.appendChild(label);
+    // add the task div to the list
+    taskList.appendChild(taskDiv);
+}
+
+function loadTask() {
+  for(let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    console.log(key);
+    let taskText = localStorage.getItem(key);
+    console.log(taskText);
+    createTask(taskText);
+  }
+}
+loadTask();
